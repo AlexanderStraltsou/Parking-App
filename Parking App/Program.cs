d89d536041626ef01b3e380513adb352b616bf66
AlexanderStraltsou <113332203+AlexanderStraltsou@users.noreply.github.com>
2024-08-29 18:42:56 +0200

    Add project files.

---
+++ Parking App/Program.cs
@@ -0,0 +1,61 @@
+﻿
+namespace ParkingApp
+{
+class TestClass
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome To Prague Castle Parking");
+            Console.WriteLine();
+
+            string[] parkingGarage = new string[100];
+            
+
+            
+            
+            Console.WriteLine("1. Park A New Vehicle");
+            Console.WriteLine("2. Move Vehicle To Another Spot");
+            Console.WriteLine("3. Remove Vehicle From Parking");
+            Console.WriteLine("4. Find A Vehicle");
+            Console.WriteLine("5. Exit");
+            Console.WriteLine();
+            Console.WriteLine("Choose Your Option By Typing In A Number And Click ENTER");
+            
+            string option = Console.ReadLine();
+
+
+            switch (option)
+            {
+                
+                case "1":
+                    break;
+
+                
+                case "2":
+                    break;
+
+               
+                case "3":
+                    break;
+
+                case "4":
+                    break;
+
+                case "5":
+                    break;
+
+
+                default:
+                    Console.WriteLine("Wrong option, try again");
+                    break;
+            }
+
+        
+            Console.WriteLine();
+
+        }
+    }
+
+}
+
